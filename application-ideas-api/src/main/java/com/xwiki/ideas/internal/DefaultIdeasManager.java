fb50dbf0ffee524fbaeeb644d4ecbfc6d9147308
AlexB <alexandru.brassat@xwiki.com>
2026-03-31 10:26:32 -0400

    Voting buttons are not updating in realtime #74 (#81)
    (cherry picked from commit 7d4adf4a665a85950a477e8ed3d466da91f458fb)

--- application-ideas-api/src/main/java/com/xwiki/ideas/internal/DefaultIdeasManager.java
+++ application-ideas-api/src/main/java/com/xwiki/ideas/internal/DefaultIdeasManager.java
@@ -190,7 +190,7 @@ public class DefaultIdeasManager implements IdeasManager
         XWikiContext xcontext = contextProvider.get();
         XWiki xWiki = xcontext.getWiki();
         try {
-            XWikiDocument ideasDoc = xWiki.getDocument(documentReference, xcontext);
+            XWikiDocument ideasDoc = xWiki.getDocument(documentReference, xcontext).clone();
 
             BaseObject ideasObj = ideasDoc.getXObject(IDEA_CLASS_REFERENCE);
             DocumentReference user = xcontext.getUserReference();
@@ -205,11 +205,9 @@ public class DefaultIdeasManager implements IdeasManager
                 } else {
                     againstAction.perform(ideasObj, serializedUser, xcontext);
                 }
-                Idea result = get(documentReference);
-
                 // Save document
                 xWiki.saveDocument(ideasDoc, "Updated Votes", xcontext);
-                return result;
+                return get(documentReference);
             } else {
                 throw new IdeasException(String.format(NOT_FOUND_ERROR, documentReference));
             }
